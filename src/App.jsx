234c35073f7d789a54966ab637dc16a6f674d71d
Selcuk Arıkan <149076617+selcuk-code-mod@users.noreply.github.com>
2024-05-03 14:51:05 +0300

    proje son hali

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,56 @@
-// import { useState } from 'react'
-
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { Button, Form, InputGroup } from "react-bootstrap";
+import styled from "styled-components";
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [todoInput, setTodoInput] = useState("");
+  const [todos, setTodos] = useState([]);
 
-  return <></>;
+  const addTodo = () => {
+    setTodos([...todos, todoInput]);
+    setTodoInput("");
+  };
+
+  const ListItem = styled.li`
+    cursor: pointer;
+  `;
+
+  return (
+    <>
+      <InputGroup className="mb-2">
+        <Form.Control
+          aria-label="Hedef"
+          aria-describedby="basic-addon2"
+          className="form"
+          onChange={(e) => {
+            setTodoInput(e.target.value);
+          }}
+          value={todoInput}
+        />
+        <Button
+          onClick={addTodo}
+          className="btn btn-primary"
+          id="button-addon2"
+        >
+          Ekle
+        </Button>
+      </InputGroup>
+      <ul>
+        {todos.map((todo) => (
+          <ListItem
+            onClick={() => {
+              setTodos(todos.filter((filterTodo) => filterTodo !== todo));
+            }}
+            key={todo}
+          >
+            {todo}
+          </ListItem>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
